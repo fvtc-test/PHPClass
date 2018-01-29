fe3af4cb6aca7a204ff1bae696eed164d89b3486
Cathy Squires <120109168@fvtc.edu>
2018-01-29 11:19:44 -0600

    Testing

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Home - PHP Class</title>
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>s
+<body>
+<div class="wrapper">
+    <header class="header">Header: Fixed height</header>
+    <section class="content">
+        <div class="columns">
+            <main class="main">Content: Flexible width</main>
+            <aside class="sidebar-first">Sidebar first: Fixed width</aside>
+        </div>
+    </section>
+    <footer class="footer">Footer: Fixed height</footer>
+</div>
+</body>
+</html
